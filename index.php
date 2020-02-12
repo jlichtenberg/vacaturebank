282ce94c4df6c397aaa3703bd2f3fb6f379ce528
jlichtenberg <lichtenberg.joel@gmail.com>
2020-02-12 16:00:45 +0100

    header css

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>PowerJobs</title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header class="site-header">
+        <div class="site-header-logo">
+            <img src="recources/play.png" alt="logo">
+            <h1>Power<span class="logo-color">Jobs</span></h1>
+        </div>
+        <nav class="main-nav">
+            <a href="">Home</a>
+            <a href="">Vacatures</a>
+            <a href="">Over ons</a>
+        </nav>
+        <nav class="account-nav">
+            <a href="">Inloggen</a>
+        </nav>
+    </header>
+        <div class="content-wrapper1">
+            <h1>Welkom bij Power<span class="logo-color">Jobs!</span></h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac erat at odio volutpat tincidunt at eu ligula. Mauris sollicitudin, urna ut pretium consequat, enim ligula interdum risus, at porttitor nibh metus vel risus. Quisque et porttitor nisl. Vivamus imperdiet ac mauris vitae pharetra. Maecenas quis nulla et urna luctus porttitor.</p>
+        </div>
+        <div class="content-wrapper2">
+            <div class="vac-box">
+                <h3>Lorem ipsum dolor sit amet</h3>
+                <div class="vac-info">
+                    <div class="vac-info-container">
+                        <img src="recources/pin.png" alt=""><p>New York</p>
+                    </div>
+                    <div class="vac-info-container">
+                        <img src="recources/clock.png" alt=""><p>22 uur</p>
+                    </div>
+                </div>
+                <p>In eget massa a nunc euismod porta eu ac ex. Proin pharetra lobortis blandit. Morbi libero dui, venenatis et ultricies in, egestas id orci. Praesent nec turpis velit.</p>
+            </div>
+
+            <div class="vac-box">
+                <h3>Lorem ipsum dolor sit amet</h3>
+                <div class="vac-info">
+                    <div class="vac-info-container">
+                        <img src="recources/pin.png" alt=""><p>New York</p>
+                    </div>
+                    <div class="vac-info-container">
+                        <img src="recources/clock.png" alt=""><p>22 uur</p>
+                    </div>
+                </div>
+                <p>In eget massa a nunc euismod porta eu ac ex. Proin pharetra lobortis blandit. Morbi libero dui, venenatis et ultricies in, egestas id orci. Praesent nec turpis velit.</p>
+            </div>
+
+            <div class="vac-box">
+                <h3>Lorem ipsum dolor sit amet</h3>
+                <div class="vac-info">
+                    <div class="vac-info-container">
+                        <img src="recources/pin.png" alt=""><p>New York</p>
+                    </div>
+                    <div class="vac-info-container">
+                        <img src="recources/clock.png" alt=""><p>22 uur</p>
+                    </div>
+                </div>
+                <p>In eget massa a nunc euismod porta eu ac ex. Proin pharetra lobortis blandit. Morbi libero dui, venenatis et ultricies in, egestas id orci. Praesent nec turpis velit.</p>
+            </div>
+
+            <div class="vac-box">
+                <h3>Lorem ipsum dolor sit amet</h3>
+                <div class="vac-info">
+                    <div class="vac-info-container">
+                        <img src="recources/pin.png" alt=""><p>New York</p>
+                    </div>
+                    <div class="vac-info-container">
+                        <img src="recources/clock.png" alt=""><p>22 uur</p>
+                    </div>
+                </div>
+                <p>In eget massa a nunc euismod porta eu ac ex. Proin pharetra lobortis blandit. Morbi libero dui, venenatis et ultricies in, egestas id orci. Praesent nec turpis velit.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
